13f8b39148046d6a19e4ce2e04ea12147f5ca105
HP <20088426@mail.wit.ie>
2022-10-31 18:40:53 +0000

    Archive a note

--- src/main/kotlin/controllers/NoteAPI.kt
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -144,6 +144,18 @@ class NoteAPI(serializerType: Serializer){
         return false
     }
 
+    fun archiveNote(indexToArchive: Int): Boolean {
+        if (isValidIndex(indexToArchive)) {
+            val noteToArchive = notes[indexToArchive]
+            if (!noteToArchive.isNoteArchived) {
+                noteToArchive.isNoteArchived = true
+                return true
+            }
+        }
+        return false
+    }
+
+
     fun isValidIndex(index: Int) :Boolean{
         return isValidListIndex(index, notes);
     }
