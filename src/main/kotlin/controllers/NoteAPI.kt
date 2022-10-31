9065f3f29648eba7d3609516001a4d880761d1f7
HP <20088426@mail.wit.ie>
2022-10-31 19:44:10 +0000

    Rewriting ListArchivedNotes to use Lambdas

--- src/main/kotlin/controllers/NoteAPI.kt
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -50,33 +50,14 @@ class NoteAPI(serializerType: Serializer){
         if  (notes.isEmpty()) "No notes stored"
         else notes.joinToString (separator = "\n") { note ->
             notes.indexOf(note).toString() + ": " + note.toString() }
-    fun listActiveNotes(): String {
-        return if (numberOfActiveNotes() == 0) {
-            "No active notes stored"
-        } else {
-            var listOfActiveNotes = ""
-            for (note in notes) {
-                if (!note.isNoteArchived) {
-                    listOfActiveNotes += "${notes.indexOf(note)}: $note \n"
-                }
-            }
-            listOfActiveNotes
-        }
-    }
 
-    fun listArchivedNotes(): String {
-        return if (numberOfArchivedNotes() == 0) {
-            "No archived notes stored"
-        } else {
-            var listOfArchivedNotes = ""
-            for (note in notes) {
-                if (note.isNoteArchived) {
-                    listOfArchivedNotes += "${notes.indexOf(note)}: $note \n"
-                }
-            }
-            listOfArchivedNotes
-        }
-    }
+    fun listArchivedNotes(): String =
+        if (numberOfArchivedNotes() == 0) "No archived notes stored"
+    else notes.filter { note -> note.isNoteArchived}
+            .joinToString ( separator = "\n") { note ->
+                notes.indexOf(note) .toString() + ": " + note.toString() }
+
+
 
     fun listNotesBySelectedPriority(priority: Int): String {
         return if (notes.isEmpty()) {
